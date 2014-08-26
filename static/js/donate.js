e57058d63bba6757e00279c9b00252586276c550
Jonathan Cobb <misterjcobb@gmail.com>
2014-08-26 15:29:34 -0700

    stuff

--- static/js/donate.js
+++ static/js/donate.js
@@ -1,8 +1,11 @@
 $('.menu-donate').click(function(){
     $('.lb').show(500, "swing");
    });
-   $('.contactbutton').click(function(){
-    $('.lb').hide(500, "swing");
+   // $('.contactbutton').click(function(){
+   //  $('.lb').hide(500, "swing");
+   // });
+$('.donateboxbutton').click(function(){
+    $('.lb').show(500, "swing");
    });
 
   $('.black-overlay').click(function(){
