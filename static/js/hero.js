6410ec92b5ae9186a2e86e34d0d38747347b1b4f
Jonathan Cobb <misterjcobb@gmail.com>
2014-08-30 17:57:35 -0700

    safari parallax

--- static/js/hero.js
+++ static/js/hero.js
@@ -2,6 +2,6 @@ var is_safari = navigator.userAgent.indexOf("Safari") > -1;
 if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) 
 {
 	
-  $('.herofix').addClass("safari");
-  $('.herospacer').addClass("safari");
+  // $('.herofix').addClass("safari");
+  // $('.herospacer').addClass("safari");
 }
