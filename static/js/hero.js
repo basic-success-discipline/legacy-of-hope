3292973a772652c8f4606a53fb9bcd7f7388f278
Jonathan Cobb <misterjcobb@gmail.com>
2014-09-22 11:17:13 -0700

    removed old files and updated deck

--- static/js/hero.js
+++
@@ -1,7 +0,0 @@
-var is_safari = navigator.userAgent.indexOf("Safari") > -1;
-if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) 
-{
-	
-  // $('.herofix').addClass("safari");
-  // $('.herospacer').addClass("safari");
-}
