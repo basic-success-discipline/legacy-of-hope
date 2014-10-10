3ce5a2f04ae730733c5128f2a6dd18695d0e2d07
Jonathan Cobb <misterjcobb@gmail.com>
2014-10-10 14:24:51 -0700

    Ben vereen

---
+++ static/js/spotlight.js
@@ -0,0 +1,29 @@
+
+//    $(document).ready(function(){
+//    	$('.banner').load(function() {
+//    	var lim = $('.banner').position().top + $('.banner').height();
+
+
+// 	$('.navbar').attr("data-" + (lim - 150).toString(), "background-color:rgb(28, 31, 38);");
+// 	$('.navbar').attr("data-" + (lim + 80).toString(), "background-color:rgb(21, 21, 21);");
+
+
+// 	// $('.hero img').attr("data-0"  + (lim - 300).toString(), "opacity:1.0;");
+// 	// $('.hero img').attr("data-" + lim, "opacity:0;");
+
+
+// 	// $('.sl .herobuffer').attr("data-0", "height:130px;margin-top: -130px;");
+// 	// $('.sl .herobuffer').attr("data-" + (lim -70).toString(), "height:200px;margin-top: -200px;");
+
+// 	// $('.sl .herobuffer').attr("data-" + lim.toString(), "opacity:0.99999;");
+// 	// $('.sl .herobuffer').attr("data-" + (lim + 100).toString(), "opacity:0;");
+
+
+// 	$('.wrapper-spotlight').attr("data-0", "margin-top:-80px;");
+// 	$('.wrapper-spotlight').attr("data-" + lim.toString(), "margin-top: 10px;");
+//     var s = skrollr.init({
+//     	forceHeight: false
+//     });
+//     });
+
+// });
