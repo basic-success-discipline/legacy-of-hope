ba2e2659cf3da1f2c676a6d2adce352228b44181
Jonathan Cobb <misterjcobb@gmail.com>
2014-10-19 21:41:46 -0500

    exec summary on hopsital

--- static/js/spotlight.js
+++ static/js/spotlight.js
@@ -1,44 +1,63 @@
 
-$(document).ready(function(){
-	$('.banner').load(function() {
-		var lim = $('.banner').position().top + $('.banner').height();
+// $(document).ready(function(){
+// 	$('.banner').load(function() {
+// 		var lim = $('.banner').position().top + $('.banner').height();
 
-		if(lim >= window.innerHeight - 170){
-			var dif = -1 * (lim - (window.innerHeight - 170)) -80;
-			var dift = .2 * dif;
-			console.log(dif);
-			window.setTimeout(function(){
-				if($(window).scrollTop() < 170){
-					if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
-						$('.buffer img').animate({
-							"margin-top": dif,
+// 		if(lim >= window.innerHeight - 170){
+// 			var dif = -1 * (lim - (window.innerHeight - 170)) -80;
+// 			var dift = .2 * dif;
+// 			console.log(dif);
+// 			window.setTimeout(function(){
+// 				if($(window).scrollTop() < 170){
+// 					if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+// 						$('.buffer img').animate({
+// 							"margin-top": dif,
 
-						}, 1000, function() {});
-
-
-						$('.wrapper-spotlight').animate({
-							"margin-top": dift
-						}, 1200, function() {
-							$('.buffer img').attr("data-100", "margin-top: " + dif + "px;");
-							$('.buffer img').attr("data-" + (lim-150).toString(), " margin-top: -80px; ");
-							$('.wrapper-spotlight').attr("data-100", "margin-top: " + dift + "px");
-							$('.wrapper-spotlight').attr("data-" + (lim-150).toString(), "margin-top: 0");
-
-							skrollr.init({
-								forceHeight: false
-							});
-
-						});
+// 						}, 1000, function() {});
 
 
-					}
-				}
-			}, 800);
-		}
+// 						$('.wrapper-spotlight').animate({
+// 							"margin-top": dift
+// 						}, 1200, function() {
+// 							$('.buffer img').attr("data-100", "margin-top: " + dif + "px;");
+// 							$('.buffer img').attr("data-" + (lim-150).toString(), " margin-top: -80px; ");
+// 							$('.wrapper-spotlight').attr("data-100", "margin-top: " + dift + "px");
+// 							$('.wrapper-spotlight').attr("data-" + (lim-150).toString(), "margin-top: 0");
 
-	});
+// 							skrollr.init({
+// 								forceHeight: false
+// 							});
+
+// 						});
 
 
-	$('.prefooter')
+// 					}
+// 				}
+// 			}, 800);
+// 		}
 
-});
+// 	});
+
+// 	// var button = $('.prefooter .donatebuttonholder button');
+	
+// 	// var goGreen = function(){
+// 	// 	console.log("green");
+// 	// 	$('.prefooter .donatebuttonholder button').animate({
+// 	// 		"background-color": '#4AAE39'
+// 	// 	}, 1000, function(){
+// 	// 		goBlue();
+// 	// 	});
+// 	// }
+
+// 	// var goBlue = function(){
+// 	// 	console.log("blue");
+// 	// 	$('.prefooter .donatebuttonholder button').animate({
+// 	// 		"background-color": '#358fE3'
+// 	// 	}, 1000, function(){
+// 	// 		goGreen();
+// 	// 	});
+// 	// }
+
+// 	// goGreen();
+
+// });
