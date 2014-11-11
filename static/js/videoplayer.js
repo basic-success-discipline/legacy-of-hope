de917bd3d1ccdc88a99bfa36bd82bc30119b1719
Jonathan Cobb <misterjcobb@gmail.com>
2014-11-11 10:19:44 -0700

    trying to work with CORS

--- static/js/videoplayer.js
+++ static/js/videoplayer.js
@@ -1,5 +1,65 @@
  akamai.amp.AMP.loadDefaults("/akamai/amp/amp.xml");
 
+
+
+
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
+}
+
+// Helper method to parse the title tag from the response.
+function getTitle(text) {
+  return text.match('<title>(.*)?</title>')[1];
+}
+
+// Make the actual CORS request.
+function makeCorsRequest(angle, giftcode) {
+  // All HTML5 Rocks properties support CORS.
+ 
+ var url = 'https://ipms-dev.appspot.com/ipms/events/LOH20141203/streams/' + angle + "/hds";
+if(giftcode && giftcode !=""){
+  url= url+"?giftcode="+giftcode;
+}
+url = 'http://updates.html5rocks.com';
+
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    var title = getTitle(text);
+    console.log("response Text: " + text);
+    alert('Response from CORS request to ' + url + ': ' + title);
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
+}
+
+
+
+
+
  var amp,
  menu = new FeedMenu(),
  giftcode = "",
@@ -7,66 +67,67 @@
  media = [];
 
  function getMedia(angle, callback){
-  $.post( "/video", { Angle: angle, GiftCode: giftcode })
-  .done(function( data ) {
+  makeCorsRequest(angle, giftcode);
+  // $.post( "/video", { Angle: angle, GiftCode: giftcode })
+  // .done(function( data ) {
 
-    var url = $.parseJSON(data).URL;
-    console.log(url);
-    media = [
-    {
-      autoplay: true,
-      title: "Demo Live Stream",
-      poster: '/akamai/resources/images/bunny.jpg',
-      temporalType: "live",
-      source: 
-      [
-      {src: url, type:"video/mp4"}
-      ],
-      mediaanalytics:
-      {
-        dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
-      }
-    }];
+  //   var url = $.parseJSON(data).URL;
+  //   console.log(url);
+  //   media = [
+  //   {
+  //     autoplay: true,
+  //     title: "Demo Live Stream",
+  //     poster: '/akamai/resources/images/bunny.jpg',
+  //     temporalType: "live",
+  //     source: 
+  //     [
+  //     {src: url, type:"video/mp4"}
+  //     ],
+  //     mediaanalytics:
+  //     {
+  //       dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
+  //     }
+  //   }];
 
-    if(premium){
-      media = [
-      {
-        autoplay: true,
-        title: "Demo Live Stream",
-        poster: '/akamai/resources/images/bunny.jpg',
-        temporalType: "live",
-        source: 
-        [
-        {src: url, type:"video/mp4"}
-        ],
-        mediaanalytics:
-        {
-          dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
-        }
-      },
+  //   if(premium){
+  //     media = [
+  //     {
+  //       autoplay: true,
+  //       title: "Demo Live Stream",
+  //       poster: '/akamai/resources/images/bunny.jpg',
+  //       temporalType: "live",
+  //       source: 
+  //       [
+  //       {src: url, type:"video/mp4"}
+  //       ],
+  //       mediaanalytics:
+  //       {
+  //         dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
+  //       }
+  //     },
 
-      {
-        autoplay: true,
-        title: "Demo Live Stream",
-        poster: '/akamai/resources/images/bunny.jpg',
-        temporalType: "live",
-        source: 
-        [
-        {src: url, type:"video/mp4"}
-        ],
-        mediaanalytics:
-        {
-          dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
-        }
-      }
-      ];
-    }
+  //     {
+  //       autoplay: true,
+  //       title: "Demo Live Stream",
+  //       poster: '/akamai/resources/images/bunny.jpg',
+  //       temporalType: "live",
+  //       source: 
+  //       [
+  //       {src: url, type:"video/mp4"}
+  //       ],
+  //       mediaanalytics:
+  //       {
+  //         dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
+  //       }
+  //     }
+  //     ];
+  //   }
 
-  }).fail(function() {
-    alert( "error" );
-  }).always(function(){
-    callback(media);
-  });
+  // }).fail(function() {
+  //   alert( "error" );
+  // }).always(function(){
+  //   callback(media);
+  // });
 }
 
 function loadHandler(event)
