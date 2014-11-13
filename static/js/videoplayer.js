ba3634a9b069cd4d88ffbd630883e2f78930f659
Jonathan Cobb <misterjcobb@gmail.com>
2014-11-13 14:18:45 -0700

    controls

--- static/js/videoplayer.js
+++ static/js/videoplayer.js
@@ -3,174 +3,73 @@
 
 
 
-// // Create the XHR object.
-// function createCORSRequest(method, url) {
-//   var xhr = new XMLHttpRequest();
-//   if ("withCredentials" in xhr) {
-//     // XHR for Chrome/Firefox/Opera/Safari.
-//     xhr.open(method, url, true);
-//   } else if (typeof XDomainRequest != "undefined") {
-//     // XDomainRequest for IE.
-//     xhr = new XDomainRequest();
-//     xhr.open(method, url);
-//   } else {
-//     // CORS not supported.
-//     xhr = null;
-//   }
-//   return xhr;
-// }
-
-// // Helper method to parse the title tag from the response.
-// function getTitle(text) {
-//   return text.match('<title>(.*)?</title>')[1];
-// }
-
-// // Make the actual CORS request.
-// function makeCorsRequest(angle, giftcode) {
-//   // All HTML5 Rocks properties support CORS.
-
-//  var url = 'https://ipms-dev.appspot.com/ipms/events/LOH20141203/streams/' + angle + "/hds";
-// if(giftcode && giftcode !=""){
-//   url= url+"?giftcode="+giftcode;
-// }
-// // url = 'http://updates.html5rocks.com';
-
-//   var xhr = createCORSRequest('GET', url);
-//   if (!xhr) {
-//     alert('CORS not supported');
-//     return;
-//   }
-
-//   // Response handlers.
-//   xhr.onload = function() {
-//     var text = xhr.responseText;
-//     var title = getTitle(text);
-//     console.log("response Text: " + text);
-//     alert('Response from CORS request to ' + url + ': ' + title);
-//   };
-
-//   xhr.onerror = function() {
-//     alert('Woops, there was an error making the request.');
-//   };
-
-//   xhr.send();
-// }
-
-
-
-
-
 var amp,
-giftcode = "",
+giftcode = "asas",
 premium = true,
-media = [
-{
-  autoplay: true,
-  title: "Demo Live Stream",
-  poster: '/akamai/resources/images/bunny.jpg',
-  temporalType: "live",
-  controls: {
-    mode: "none"
-  },
-  source: 
-  [
-  {src: "http://multiformatlive-f.akamaihd.net/z/demostream_1@2131/manifest.f4m", type: "video/f4m"},
-  {src: "http://multiformatlive-f.akamaihd.net/i/demostream_1@2131/master.m3u8", type: "application/x-mpegURL"}
-  ],
-  mediaanalytics:
-  {
-    dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
-  }
-},
+media = [];
 
-{
-  autoplay: true,
-  title: "Demo Live Stream",
-  poster: '/akamai/resources/images/bunny.jpg',
-  temporalType: "live",
-  controls: {
-    mode: "none"
-  },
-  source: 
-  [
-  {src: "http://multiformatlive-f.akamaihd.net/z/demostream_1@2131/manifest.f4m", type: "video/f4m"},
-  {src: "http://www.nasa.gov/multimedia/nasatv/NTV-Public-IPS.m3u8", type: "application/x-mpegURL"}
-  ],
-  mediaanalytics:
-  {
-    dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
+
+
+
+ function getMedia(angle, callback) {
+   var url = 'https://ipms-dev.appspot.com/ipms/events/LOH20141203/streams/' + angle + "/hds?zotz=161803";
+   if(giftcode && giftcode !="" && angle == 2){
+    url= url+"&giftcode="+giftcode;
+  }else if (angle==2){
+    alert("need a giftcode!");
   }
+
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    alert('CORS not supported');
+    return;
+  }
+
+  // Response handlers.
+  xhr.onload = function() {
+    var text = xhr.responseText;
+    var data = JSON.parse(text);
+    console.log(data.URL);
+    media=[
+    {
+      autoplay: true,
+      title: "Demo Live Stream",
+      poster: '/akamai/resources/images/bunny.jpg',
+      temporalType: "live",
+      controls: {
+        mode: "none"
+      },
+      source: 
+      [
+      {src: data.URL, type: "video/mp4"}
+      ],
+      mediaanalytics:
+      {
+        dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
+      }
+    },
+    ];
+
+    callback(media);
+  };
+
+  xhr.onerror = function() {
+    alert('Woops, there was an error making the request.');
+  };
+
+  xhr.send();
 }
-];
 
- // function getMedia(angle, callback){
-  // makeCorsRequest(angle, giftcode);
-  // $.post( "/video", { Angle: angle, GiftCode: giftcode })
-  // .done(function( data ) {
 
-  //   var url = $.parseJSON(data).URL;
-  //   console.log(url);
-  //   media = [
-  //   {
-  //     autoplay: true,
-  //     title: "Demo Live Stream",
-  //     poster: '/akamai/resources/images/bunny.jpg',
-  //     temporalType: "live",
-  //     source: 
-  //     [
-  //     {src: url, type:"video/mp4"}
-  //     ],
-  //     mediaanalytics:
-  //     {
-  //       dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
-  //     }
-  //   }];
 
-  //   if(premium){
-  //     media = [
-  //     {
-  //       autoplay: true,
-  //       title: "Demo Live Stream",
-  //       poster: '/akamai/resources/images/bunny.jpg',
-  //       temporalType: "live",
-  //       source: 
-  //       [
-  //       {src: url, type:"video/mp4"}
-  //       ],
-  //       mediaanalytics:
-  //       {
-  //         dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
-  //       }
-  //     },
 
-  //     {
-  //       autoplay: true,
-  //       title: "Demo Live Stream",
-  //       poster: '/akamai/resources/images/bunny.jpg',
-  //       temporalType: "live",
-  //       source: 
-  //       [
-  //       {src: url, type:"video/mp4"}
-  //       ],
-  //       mediaanalytics:
-  //       {
-  //         dimensions: { title: "TITLE3", eventName: "EVENTNAME3" }
-  //       }
-  //     }
-  //     ];
-  //   }
 
-  // }).fail(function() {
-  //   alert( "error" );
-  // }).always(function(){
-  //   callback(media);
-  // });
-// }
+
+
 
 function loadHandler(event)
 {
-
-  // getMedia(1, function(media){
+  getMedia(1, function(media){
     var config = 
     {
       rules:
@@ -184,14 +83,16 @@ function loadHandler(event)
     };
 
     amp = new akamai.amp.AMP(document.getElementById("akamai-media-player"), config, readyHandler);
+    
+    amp.addEventListener("pause", togglePlayButton(false));
+    amp.addEventListener("play", togglePlayButton(true));
 
-  // });
+  });
 
 }
 
 function readyHandler(event)
 {
-
   amp.addEventListener("ended", endedHandler);
   loadVideo(0);
 }
@@ -199,16 +100,14 @@ function readyHandler(event)
 
 function loadVideo(index)
 {
-
-  // getMedia(index+1, function(media){
-    console.log(media[index]);
-    amp.setMedia(media[index]);
-  // });
+  getMedia(index+1, function(media){
+    console.log(media[0]);
+    amp.setMedia(media[0]);
+  });
 }
 
 function endedHandler(event)
 {
-
   console.log("ended");
 }
 
@@ -222,16 +121,58 @@ playerWrapper: '.video-player',
 
 });
 
-
+function togglePlayButton(isPlaying){
+  if(isPlaying){
+  $('.playbutton').removeClass('fa-play');
+  $('.playbutton').addClass('fa-pause');
+}else{
+  $('.playbutton').removeClass('fa-pause');
+  $('.playbutton').addClass('fa-play');
+}
+}
 
 function playPause(){
  if(amp.getPaused()){
   amp.play();
+  togglePlayButton(true);
 }else{
  amp.pause();
+togglePlayButton(false);
 }
 }
 
 function setVolume(value){
   amp.setVolume(value/20);
+}
+
+function toggleCS(){
+  if($('#crowdsurfing-wrapper').is(":visible")){
+  $('#crowdsurfing-wrapper').hide();
+  $('.csBtn').html('Show Crowdsurfing');
+}else{
+  $('#crowdsurfing-wrapper').show();
+  $('.csBtn').html('Hide Crowdsurfing');
+}
+}
+
+
+
+
+
+
+// Create the XHR object.
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+    // XHR for Chrome/Firefox/Opera/Safari.
+    xhr.open(method, url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    // XDomainRequest for IE.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    // CORS not supported.
+    xhr = null;
+  }
+  return xhr;
 }
